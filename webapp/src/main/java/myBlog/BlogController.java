37baec24b2cff15e35bfab4ffaee4d4271dbbef1
root <dmlaomao@gmail.com>
2016-03-26 22:16:37 +0800

    link to mysql with JPA

--- webapp/src/main/java/myBlog/BlogController.java
+++ webapp/src/main/java/myBlog/BlogController.java
@@ -1,21 +1,26 @@
 package myBlog;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import com.google.gson.*;
 
 @RestController
+@EnableAutoConfiguration
+
 public class BlogController {
 
-    //private static final String template = "Hello, %s!";
-    //private final AtomicLong counter = new AtomicLong();
-    private static Dao dao = new Dao();
+    //private static Dao dao = new Dao();
+    //private static BlogRepository blogRepo = new BlogRepository();
+    @Autowired
+    private BlogRepository blogRepo; 
     private static Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 
     @RequestMapping("/blog")
     public String blog(@RequestParam(value="ID", defaultValue = "1") long id) {
-        return gson.toJson(dao.getBlogByID(id));
+        return gson.toJson(blogRepo.findByid(id));
+        //return gson.toJson(BlogRepository.findByid(id));
     }
 }
---
+++ webapp/src/main/java/myBlog/BlogRepository.java
@@ -0,0 +1,9 @@
+package myBlog;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BlogRepository extends CrudRepository<Blog, Long> {
+    public Blog findByid(long id);
+}
