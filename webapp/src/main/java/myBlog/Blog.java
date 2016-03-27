6077704d5bb007e832900fb93dcd93b34cc0fe27
root <dmlaomao@gmail.com>
2016-03-27 14:42:21 +0800

    add jpa annotation

--- webapp/src/main/java/myBlog/Blog.java
+++ webapp/src/main/java/myBlog/Blog.java
@@ -1,21 +1,39 @@
 package myBlog;
 
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "Blog")
 public class Blog {
 
-    private final long id;
+    @Id
+    @GeneratedValue        
+    private long id;
     //the format?
-    //private final time;
-    private final String title;
-    private final String content;
+    private Timestamp time;
+    private String title;
+    private String content;
 
-    public Blog(long id, String title, String content) {
+    protected Blog() {}
+
+    public Blog(long id, Timestamp time,String title, String content) {
         this.id = id;
+        this.time = time;
         this.title = title;
         this.content = content;
     }
 
     public long getId() {
         return id;
+    }
+
+    public Timestamp getTime() {
+        return time;
     }
 
     public String getTitle() {
