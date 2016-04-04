d017e4b706c1ee3d3cd6362655502e880a9ac8aa
root <dmlaomao@gmail.com>
2016-04-04 23:42:14 +0800

    change ajax async to false, which fix the login with no reply problem

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ jQuery(document).ready(function($){
 	function login_selected(){
 		$form_login.addClass('is-selected');
 		$form_signup.removeClass('is-selected');
-		$form_forgot_password.removeClass('is-selected');
+		//$form_forgot_password.removeClass('is-selected');
 		$tab_login.addClass('selected');
 		$tab_signup.removeClass('selected');
 	}
@@ -54,7 +54,7 @@ jQuery(document).ready(function($){
 	function signup_selected(){
 		$form_login.removeClass('is-selected');
 		$form_signup.addClass('is-selected');
-		$form_forgot_password.removeClass('is-selected');
+		//$form_forgot_password.removeClass('is-selected');
 		$tab_login.removeClass('selected');
 		$tab_signup.addClass('selected');
 	}
