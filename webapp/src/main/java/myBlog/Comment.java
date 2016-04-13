8f25020f5d67f9d9bf331cbda9943a95972d6f46
root <dmlaomao@gmail.com>
2016-04-13 09:46:07 +0800

    add Comment bean

---
+++ webapp/src/main/java/myBlog/Comment.java
@@ -0,0 +1,52 @@
+package myBlog;
+
+import java.sql.Timestamp;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue        
+    private long id;
+    //the format?
+    private String content;
+    private Timestamp time;
+    private String name;
+    private int blogid;
+
+    protected Blog() {}
+
+    public Blog(/*long id,*/ Timestamp time,String content, String name, int blogid) {
+        //this.id = id;
+        this.time = time;
+        this.content = content;
+        this.name = name;
+        this.blogid = blogid;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBlogID() {
+        return blogid;
+    }
+}
