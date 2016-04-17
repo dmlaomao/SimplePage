72935439665ced8767b8de6147be4d9cab92d685
root <dmlaomao@gmail.com>
2016-04-17 20:29:16 +0800

    fix bug

--- webapp/src/main/java/myBlog/CommentRepository.java
+++ webapp/src/main/java/myBlog/CommentRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends CrudRepository<User, String> {
+public interface CommentRepository extends CrudRepository<Comment, String> {
     public Comment findByBlogid(int blogid);
     public Comment save(Comment comment);
 }
