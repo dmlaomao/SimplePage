bfec1c57a62b0a572ce702b4c354b2a83df656dd
root <dmlaomao@gmail.com>
2016-04-18 16:10:13 +0800

    fix the no list return error

--- webapp/src/main/java/myBlog/CommentRepository.java
+++ webapp/src/main/java/myBlog/CommentRepository.java
@@ -1,10 +1,11 @@
 package myBlog;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends CrudRepository<Comment, String> {
-    public Comment findByBlogid(int blogid);
+    public List<Comment> findByBlogid(int blogid);
     public Comment save(Comment comment);
 }
