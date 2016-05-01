e736ac3d9c91aee7aa0990c626af174e4d504599
root <dmlaomao@gmail.com>
2016-05-01 22:15:41 +0800

    add redis cache on comment

--- webapp/src/main/java/myBlog/CommentController.java
+++ webapp/src/main/java/myBlog/CommentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import com.google.gson.*;
 
 @RestController
@@ -17,18 +18,46 @@ public class CommentController {
 
     @Autowired
     private CommentRepository commentRepo; 
+    @Autowired
+    private StringRedisTemplate redis; 
 
     @RequestMapping("/getComment")
     public String login(@RequestParam(value="blogid") int blogid) {
         //return all comments of the given blog
-        return gson.toJson(commentRepo.findByBlogid(blogid));
+        //add redis here
+        String strid = String.valueOf(blogid);
+        //timer
+        long bef = System.currentTimeMillis();
+        String redisStr = redis.opsForValue().get(strid);
+        long aft = System.currentTimeMillis();
+        aft -= bef;
+        if (redisStr == null) {
+            bef = System.currentTimeMillis();
+
+            redisStr = gson.toJson(commentRepo.findByBlogid(blogid));
+
+            aft = System.currentTimeMillis();
+            aft -= bef;
+
+            redis.opsForValue().set(strid, redisStr);
+            System.out.println("no hit" + " time " + aft);
+        }
+        else 
+            System.out.println("hit!!" + " time " + aft);
+
+        return redisStr;
     }
 
     @RequestMapping("/saveComment")
-    public String register(@RequestParam(value="content") String content, @RequestParam(value="token") String token, @RequestParam(value="blogid") int id) {
+    public String register(@RequestParam(value="content") String content, @RequestParam(value="token") String token, @RequestParam(value="blogid") int blogid) {
         try {
-            Comment c = new Comment(new Timestamp(System.currentTimeMillis()), content, LogTemp.findByToken(token), id); 
+            Comment c = new Comment(new Timestamp(System.currentTimeMillis()), content, LogTemp.findByToken(token), blogid); 
             commentRepo.save(c);
+            //add redis here
+            String redisStr = gson.toJson(commentRepo.findByBlogid(blogid));
+            redis.opsForValue().set(String.valueOf(blogid), redisStr);
+            System.out.println("cache update");
+
         } catch (Exception e) {
             return "false";
         }
